1ae6bef1c29626b2c17b9bac36d3b913e437ecc8
vincent li <v1nc3ntl1@gmail.com>
2019-09-22 07:10:36 +1000

    changes 1

--- VincentFuncApp1/Function1.cs
+++ VincentFuncApp1/Function1.cs
@@ -29,7 +29,7 @@ namespace VincentFuncApp1
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name + ". You are super AWESOME!");
         }
     }
 }
